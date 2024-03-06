cf485155718ec7e370ba6a7c7571ff395c6c8ff8
arief-ak <mariefak22@gmail.com>
2024-03-06 17:26:45 +0100

    updated workflow to install colcon-common-extensions

--- .github/workflows/install_dependencies.sh
+++ .github/workflows/install_dependencies.sh
@@ -8,9 +8,14 @@ locale
 echo "${GITHUB_ACTOR} has set up ubuntu with updated locale config"
 sudo apt install software-properties-common
 sudo add-apt-repository universe
+echo "${GITHUB_ACTOR} has updated ubuntu repositories"
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+echo "${GITHUB_ACTOR} has pulled ROS2 repositories"
 sudo apt update && sudo apt install ros-dev-tools
+sudo apt update && sudo apt install python3-colcon-common-extensions
+echo "${GITHUB_ACTOR} has installed ROS and Python3 tools"
 sudo apt update
 sudo apt upgrade
+echo "${GITHUB_ACTOR} has updated and upgraded Ubuntu"
