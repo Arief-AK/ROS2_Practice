f41479702d79a78c407796894828946787e5238f
arief-ak <mariefak22@gmail.com>
2024-03-06 17:43:56 +0100

    updated workflow to install colcon-common-extensions

--- .github/workflows/install_dependencies.sh
+++ .github/workflows/install_dependencies.sh
@@ -14,8 +14,7 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 echo "${GITHUB_ACTOR} has pulled ROS2 repositories"
 sudo apt update && sudo apt install ros-dev-tools
-sudo apt update && sudo apt install python3-colcon-common-extensions
-echo "${GITHUB_ACTOR} has installed ROS and Python3 tools"
+echo "${GITHUB_ACTOR} has installed ROS tools"
 sudo apt update
 sudo apt upgrade
 echo "${GITHUB_ACTOR} has updated and upgraded Ubuntu"
