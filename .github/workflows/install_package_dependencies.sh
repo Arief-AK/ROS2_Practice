a5bdcbd67d3b12d35c6ef38026223c41b21887c2
arief-ak <mariefak22@gmail.com>
2024-03-08 12:17:40 +0100

    testing ros setup on docker image

--- .github/workflows/install_package_dependencies.sh
+++ .github/workflows/install_package_dependencies.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-echo "${GITHUB_ACTOR} is attempting to install ROS2 dependencies"
 sudo apt-get update && sudo apt-get install python3-colcon-common-extensions
 which colcon && which rosdep
 sudo rosdep init && rosdep update
-rosdep install -i --from-path src --rosdistro iron -y
-echo "${GITHUB_ACTOR} has installed ROS2 dependencies"
+rosdep "install -i --from-path src --rosdistro ${ROS_DISTRO} -y"
--- .github/workflows/install_ros.sh
+++ .github/workflows/install_ros.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-echo "${GITHUB_ACTOR} is attempting to install ROS2"
 sudo apt install ros-iron-desktop
-source /opt/ros/iron/setup.bash && ros2 run --help
-echo "$GITHUB_ACTOR successfully installed ROS2-iron in system"
+source "/opt/ros/${ROS_DISTRO}/setup.bash && ros2 run --help"
