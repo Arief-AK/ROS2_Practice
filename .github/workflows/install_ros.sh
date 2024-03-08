d55665be45df950e5b0c040eeb8d54b73fb31cc7
arief-ak <mariefak22@gmail.com>
2024-03-08 17:09:33 +0100

    updated install_ros shell script for bare metal

--- .github/workflows/install_ros.sh
+++ .github/workflows/install_ros.sh
@@ -5,5 +5,3 @@ ROS_DISTRO=$2
 
 echo "${GITHUB_ACTOR} is attempting to install ROS2"
 sudo apt install ros-${ROS_DISTRO}-desktop
-source ./opt/ros/${ROS_DISTRO}/setup.bash && ros2 run --help
-echo "$GITHUB_ACTOR successfully installed ROS2-$ROS_DISTRO in system"
