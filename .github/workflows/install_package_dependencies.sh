06e783b6ba7e8bedc1bbcf4638e0f3395f761d61
arief-ak <mariefak22@gmail.com>
2024-03-08 16:48:26 +0100

    testing ros setup with iron on bare metal

--- .github/workflows/install_package_dependencies.sh
+++ .github/workflows/install_package_dependencies.sh
@@ -7,5 +7,5 @@ echo "${GITHUB_ACTOR} is attempting to install ROS2 dependencies"
 sudo apt-get update && sudo apt-get install python3-colcon-common-extensions
 which colcon && which rosdep
 sudo rosdep init && rosdep update
-rosdep "install -i --from-path src --rosdistro ${ROS_DISTRO} -y"
+rosdep install -i --from-path src --rosdistro ${ROS_DISTRO} -y
 echo "${GITHUB_ACTOR} has installed ROS2 dependencies"
--- .github/workflows/install_ros.sh
+++ .github/workflows/install_ros.sh
@@ -4,6 +4,6 @@ GITHUB_ACTOR=$1
 ROS_DISTRO=$2
 
 echo "${GITHUB_ACTOR} is attempting to install ROS2"
-sudo "apt install ros-${ROS_DISTRO}-desktop"
-source "/opt/ros/${ROS_DISTRO}/setup.bash" && ros2 run --help
+sudo apt install ros-${ROS_DISTRO}-desktop
+source /opt/ros/${ROS_DISTRO}/setup.bash && ros2 run --help
 echo "$GITHUB_ACTOR successfully installed ROS2-$ROS_DISTRO in system"
