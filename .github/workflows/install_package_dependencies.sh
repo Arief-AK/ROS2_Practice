430300ebee35a5e07abdeaf57e0f37ccb889905d
arief-ak <mariefak22@gmail.com>
2024-03-08 16:13:55 +0100

    testing ros setup with iron on bare metal

--- .github/workflows/install_package_dependencies.sh
+++ .github/workflows/install_package_dependencies.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# Extracting arguments
+ROS_DISTRO=$1
+
 sudo apt-get update && sudo apt-get install python3-colcon-common-extensions
 which colcon && which rosdep
 sudo rosdep init && rosdep update
--- .github/workflows/install_ros.sh
+++ .github/workflows/install_ros.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+# Extracting arguments
+ROS_DISTRO=$1
+
 sudo "apt install ros-${ROS_DISTRO}-desktop"
 source "/opt/ros/${ROS_DISTRO}/setup.bash && ros2 run --help"
