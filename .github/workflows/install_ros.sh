5ba59e9eb98a507f7f920c52f6b2b03fbf0094b5
arief-ak <mariefak22@gmail.com>
2024-03-08 16:40:50 +0100

    testing ros setup with iron on bare metal

--- .github/workflows/install_ros.sh
+++ .github/workflows/install_ros.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-sudo "apt install ros-${ROS_VERSION}-desktop"
-source "/opt/ros/${ROS_VERSION}/setup.bash && ros2 run --help"
+
+GITHUB_ACTOR=$1
+ROS_DISTRO=$2
+
+echo "${GITHUB_ACTOR} is attempting to install ROS2"
+sudo "apt install ros-${ROS_DISTRO}-desktop"
+source "/opt/ros/${ROS_DISTRO}/setup.bash" && ros2 run --help
+echo "$GITHUB_ACTOR successfully installed ROS2-$ROS_DISTRO in system"
