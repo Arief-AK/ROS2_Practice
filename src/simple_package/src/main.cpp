44bf52343605a9e03f4830eb3a409af3a08fcb0a
arief-ak <mariefak22@gmail.com>
2024-03-20 13:32:41 +0100

    created a simple package for using GTest

---
+++ src/simple_package/src/main.cpp
@@ -0,0 +1,10 @@
+#include "../include/simple_package/Queue.hpp"
+
+#include <iostream>
+#include <gtest/gtest.h>
+
+// Main function - can use terminal arguments as well
+int main(){
+    std::cout << "Hello, from simple package!\n";
+    return 0;
+}
