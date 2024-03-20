7fef38bd17a9c45f059fbb6dd1c1a029535d6d00
arief-ak <mariefak22@gmail.com>
2024-03-20 13:08:08 +0100

    modified to work with GTest dependency

--- src/cpp_arief_pubsub/src/main.cpp
+++ src/cpp_arief_pubsub/src/main.cpp
@@ -1,10 +1,35 @@
-#include <gtest/gtest.h>
-
+#include <cstring>
 #include "../include/cpp_arief_pubsub/Publisher.hpp"
 #include "../include/cpp_arief_pubsub/Subscriber.hpp"
 
 int main(int argc, char ** argv)
 {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  // Check for command-line arguments
+  auto i = 0;
+  auto found = false;
+
+  while (i < argc)
+  {
+    if(strcmp(argv[i], "-talker") == 0){
+      std::cout << "Publisher running..." << std::endl;
+      rclcpp::init(argc, argv);
+      rclcpp::spin(std::make_shared<MinimalPublisher>());
+      rclcpp::shutdown();
+      found = true;
+    }else if (strcmp(argv[i], "-listener") == 0)
+    {
+      std::cout << "Subscriber running..." << std::endl;
+      rclcpp::init(argc, argv);
+      rclcpp::spin(std::make_shared<MinimalSubscriber>());
+      rclcpp::shutdown();
+      found = true;
+    }
+    i++;
+  }
+
+  if(!found){
+    std::cout << "Please enter -talker or -listener argument" << std::endl;
+  }
+
+  return 0;
 }
