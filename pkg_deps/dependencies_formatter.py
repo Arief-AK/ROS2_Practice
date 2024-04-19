050d099b1ed016ec8992b52b6b92b61187e41b52
arief-ak <mariefak22@gmail.com>
2024-04-19 15:50:25 +0200

    updated to have argument parsing and priting of ros2 pkg

--- pkg_deps/dependencies_formatter.py
+++ pkg_deps/dependencies_formatter.py
@@ -1,10 +1,35 @@
 # Imports
 import os
+import argparse
+from dataclasses import dataclass
 
 # Global
-input_file = f"{os.getcwd()}/packages.txt"
+parser = argparse.ArgumentParser(description="Script to format dependencies and output them into separate text-files.")
+parser.add_argument("-a", "--append", help="Append to the dependencies", action='store_true')
+parser.add_argument("-c", "--clean", help="clean text-files for parsing", action="store_true")
+parser.add_argument("file", help="the file", type=str, default="packages.txt")
 
-def format_text() -> None:
+@dataclass
+class Package:
+    name: str
+    type: str
+    deps: list
+
+def create_ros_pkg_text(package: Package) -> str:
+    # Format the dependencies
+    dependencies = ""
+    for dep in package.deps:
+        dependencies += f"{dep} "
+
+    # Go through the dependencies
+    if package.type == "ros.ament_cmake":
+        ros_string = f"ros2 pkg create --build-type ament_cmake --license Apache-2.0 {package.name} --dependencies {package.deps}"
+
+    return ros_string
+
+def clean_file(file:str) -> None:
+    input_file = f"{os.getcwd()}/{file}"
+    
     # Read lines from the input file and append "text" to each line
     with open(input_file, 'r') as file:
         lines = file.readlines()
@@ -24,5 +49,42 @@ def format_text() -> None:
 
     print(f"Overwriten packages.txt to contain only package names")
 
+def append_file(file:str):
+    input_file = f"{os.getcwd()}/{file}"
+    
+    # Read lines from the input file and append "text" to each line
+    with open(input_file, 'r') as file:
+        packages = file.readlines()
+
+    for package in packages:
+        package = package.rstrip("\n")
+        input_file = f"{os.getcwd()}/{package}_packages.txt"
+        print(f"Opening the {package}_packages.txt file")
+        
+        with open(input_file, 'r') as package_file:
+            package_file_lines = package_file.readlines()
+        
+        for line in package_file_lines:
+            if "name:" in line:
+                name = line.split(":")[1].strip()
+            elif "type:" in line:
+                type = line.split(":")[1].strip()
+            elif "build:" in line:
+                deps = line.split(":")[1].strip()
+
+        # Create a new package
+        new_package = Package(name,type,deps)
+        ros_string = create_ros_pkg_text(new_package)
+
+        with open(input_file, "a") as package_file:
+            package_file.write(f"\nUSE THE COMMAND BELOW TO (RE)-CREATE THIS PACKAGE:")
+            package_file.write(f"\n{ros_string}")
+        
+
 if __name__ == "__main__":
-    format_text()
+    arguments = parser.parse_args()
+    if arguments.clean:
+        clean_file(arguments.file)
+    elif arguments.append:
+        print(arguments.file)
+        append_file(arguments.file)
